9553f55490f37a7f9e922b9903294b7ca33fcad1
nischal-knl <nischal.khanal36@gmail.com>
2026-01-21 08:28:44 +0545

    Portfolio design and developing

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Web Designer', 'UI/UX Enthusiast'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
